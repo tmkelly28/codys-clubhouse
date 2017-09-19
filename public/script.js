ee0ac52c2b6802e5ea91c0b1bc1f59712a1db3b7
Thomas Kelly <tmkelly28@gmail.com>
2017-09-18 22:12:38 -0400

    set up animation on splash page

--- public/script.js
+++ public/script.js
@@ -1,1 +1,23 @@
-console.log('Hello!')
+(function ($) {
+  var quips = [
+    'Get married',
+    'Are really excited about filing jointly',
+    'Totally aren\'t being forced to do this'
+  ]
+  var len = quips.length
+  var max = len * 10
+  var slow = 3000
+
+  function main (el, idx) {
+    el.text(quips[idx % len])
+    el.fadeIn(slow, function () {
+      el.fadeOut(slow, function () {
+        main(el, idx >= max ? 0 : idx + 1)
+      })
+    })
+  }
+
+  $(function () {
+    main($('#fader'), 0)
+  })
+})(window.$)
