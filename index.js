48be4bad237ff0c367d89dfa296e05466bb4a5dd
Thomas Kelly <tmkelly28@gmail.com>
2017-09-19 11:04:00 -0400

    added rsvp button and fixed some other styles and added map

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const nunjucks = require('nunjucks')
+const axios = require('axios')
 const PORT = process.env.PORT || 8080
 const DEV = process.env.NODE_ENV === 'development'
 
 nunjucks.configure('views')
+
+if (process.env.NODE_ENV !== 'production') require('./env')
+const mapUrl = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_KEY}&callback=initMap`
 
 module.exports = express()
   .use(morgan('dev'))
@@ -14,6 +18,14 @@ module.exports = express()
   .use(bodyParser.urlencoded({ extended: true }))
   .set('view engine', 'html')
   .engine('html', nunjucks.render)
+  .get('/google-maps', (req, res, next) => {
+    axios.get(mapUrl)
+      .then(({data}) => {
+        res.setHeader('Content-Type', 'application/javascript')
+        res.send(data)
+      })
+      .catch(next)
+  })
   .use(express.static(join(__dirname, 'public')))
   .use('*', (req, res) => res.render('layout'))
   .use((err, req, res, next) => {
---
+++ public/map.js
@@ -0,0 +1,83 @@
+function initMap () {
+  var gowanus = {lat: 40.6704531, lng: -74.0152036}
+  var styles = [
+    {
+      'featureType': 'landscape.natural',
+      'elementType': 'geometry.fill',
+      'stylers': [
+        {
+          'visibility': 'on'
+        },
+        {
+          'color': '#e0efef'
+        }
+      ]
+    },
+    {
+      'featureType': 'poi',
+      'elementType': 'geometry.fill',
+      'stylers': [
+        {
+          'visibility': 'on'
+        },
+        {
+          'hue': '#1900ff'
+        },
+        {
+          'color': '#c0e8e8'
+        }
+      ]
+    },
+    {
+      'featureType': 'road',
+      'elementType': 'geometry',
+      'stylers': [
+        {
+          'lightness': 100
+        },
+        {
+          'visibility': 'simplified'
+        }
+      ]
+    },
+    {
+      'featureType': 'road',
+      'elementType': 'labels',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'transit.line',
+      'elementType': 'geometry',
+      'stylers': [
+        {
+          'visibility': 'on'
+        },
+        {
+          'lightness': 700
+        }
+      ]
+    },
+    {
+      'featureType': 'water',
+      'elementType': 'all',
+      'stylers': [
+        {
+          'color': '#7dcdcd'
+        }
+      ]
+    }
+  ]
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: gowanus,
+    styles: styles
+  })
+  var marker = new google.maps.Marker({
+    position: gowanus,
+    map: map
+  })
+}
