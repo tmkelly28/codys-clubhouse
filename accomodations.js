4b42407e95d987e4eb62c011ec6455aca95f96ad
Thomas Kelly <tmkelly28@gmail.com>
2017-09-19 12:10:31 -0400

    start availing myself of nunjucks

---
+++ accomodations.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    name: 'Place A',
+    address: 'This is the address',
+    city: 'Brooklyn, NY',
+    phone: '(609)-867-5309',
+    href: 'https://www.google.com/maps/place/Holiday+Inn+Express+New+York-Brooklyn/@40.6781893,-73.9846578,19.34z/data=!4m5!3m4!1s0x0:0xec1d8876a95786c7!8m2!3d40.678161!4d-73.9844307'
+  },
+  {
+    name: 'Place B',
+    address: 'This is the address',
+    city: 'Brooklyn, NY',
+    phone: '(609)-867-5309',
+    href: 'https://www.google.com/maps/place/Holiday+Inn+Express+New+York-Brooklyn/@40.6781893,-73.9846578,19.34z/data=!4m5!3m4!1s0x0:0xec1d8876a95786c7!8m2!3d40.678161!4d-73.9844307'
+  },
+  {
+    name: 'Place C',
+    address: 'This is the address',
+    city: 'Brooklyn, NY',
+    phone: '(609)-867-5309',
+    href: 'https://www.google.com/maps/place/Holiday+Inn+Express+New+York-Brooklyn/@40.6781893,-73.9846578,19.34z/data=!4m5!3m4!1s0x0:0xec1d8876a95786c7!8m2!3d40.678161!4d-73.9844307'
+  }
+]
--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const nunjucks = require('nunjucks')
+const compression = require('compression')
 const axios = require('axios')
+const accomodations = require('./accomodations')
 const PORT = process.env.PORT || 8080
 const DEV = process.env.NODE_ENV === 'development'
+const MAP_URL = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_KEY}&callback=initMap`
 
 nunjucks.configure('views')
 
 if (process.env.NODE_ENV !== 'production') require('./env')
-const mapUrl = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_KEY}&callback=initMap`
+
 
 module.exports = express()
   .use(morgan('dev'))
@@ -18,8 +21,9 @@ module.exports = express()
   .use(bodyParser.urlencoded({ extended: true }))
   .set('view engine', 'html')
   .engine('html', nunjucks.render)
+  .use(compression())
   .get('/google-maps', (req, res, next) => {
-    axios.get(mapUrl)
+    axios.get(MAP_URL)
       .then(({data}) => {
         res.setHeader('Content-Type', 'application/javascript')
         res.send(data)
@@ -27,7 +31,7 @@ module.exports = express()
       .catch(next)
   })
   .use(express.static(join(__dirname, 'public')))
-  .use('*', (req, res) => res.render('layout'))
+  .use('*', (req, res) => res.render('index', {accomodations}))
   .use((err, req, res, next) => {
     console.error(err)
     console.error(err.stack)
